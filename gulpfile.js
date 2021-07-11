b96d8a2c3fe92542124d91c018ec3d5421267eea
Bamboo_Run <1712013378@qq.com>
2021-07-11 22:31:07 +0800

    增加样式和结构

--- gulpfile.js
+++ gulpfile.js
@@ -1,79 +1,71 @@
-const {
-    series,
-    src,
-    dest,
-    watch
-} = require('gulp');
+const { series, src, dest, watch } = require('gulp');
 
 const htmlClean = require('gulp-htmlclean');
 
 const less = require('gulp-less');
-const cssClean = require('gulp-clean-css');
+const cleanCss = require('gulp-clean-css');
 
-const stripeDebug = require('gulp-strip-debug');
-const uglifyEs = require('gulp-uglify-es').default;
+const stripDebug = require('gulp-strip-debug');
+const uglify = require('gulp-uglify');
 
-const imageMin = require('gulp-imagemin');
+const imgMin = require('gulp-imagemin');
 
 const connect = require('gulp-connect');
-const {
-    reload
-} = require('gulp-connect');
 
 const folder = {
-    src: 'src/',
-    dist: 'dist/'
+	src: 'src/',
+	dist: 'dist/'
 }
 
 function html() {
-    return src(folder.src + 'html/*')
-        .pipe(htmlClean())
-        .pipe(dest(folder.dist + 'html/'))
-        .pipe(reload());
-}
-// 先处理less为css->压缩css
-function css(cb) {
-    return src(folder.src + 'css/*')
-        .pipe(less())
-        .pipe(cssClean())
-        .pipe(dest(folder.dist + 'css/'))
-        .pipe(reload());
-    cb();
+	return src(folder.src + 'html/*')
+		.pipe(htmlClean())
+		.pipe(dest(folder.dist + 'html/'))
+		.pipe(connect.reload());
 }
 
-function js(cb) {
-    return src(folder.src + 'js/*')
-        .pipe(stripeDebug())
-        .pipe(uglifyEs())
-        .pipe(dest(folder.dist + 'js/'))
-        .pipe(reload());
-    cb();
+function css() {
+	return src(folder.src + 'css/*')
+		.pipe(less())
+		.pipe(cleanCss())
+		.pipe(dest(folder.dist + 'css/'))
+		.pipe(connect.reload());
 }
 
-function image(cb) {
-    return src(folder.src + 'images/*')
-        .pipe(imageMin())
-        .pipe(dest(folder.dist + 'images/'));
-    cb();
+function js() {
+	return src(folder.src + 'js/*')
+		.pipe(stripDebug())
+		.pipe(uglify())
+		.pipe(dest(folder.dist + 'js/'))
+		.pipe(connect.reload());
 }
 
-// 开启服务器，监听文件变化
-function server() { //导入connect，写入server任务
-    connect.server({
-        port: '1573',
-        livereload: true //自动刷新
-    });
+function image() {
+	return src(folder.src + 'images/*')
+		.pipe(imgMin())
+		.pipe(dest(folder.dist + 'images/'))
 }
-watch(folder.src + 'html/', function (cb) {
-    html();
-    cb();
-})
-watch(folder.src + 'css/', function (cb) {
-    css();
-    cb();
-})
-watch(folder.src + 'js/', function (cb) {
-    js();
-    cb();
-})
-exports.default = series(html, css, js, image, server);
+
+function server(cb) {
+	connect.server({
+		port: '1573',
+		livereload: true,	//自动刷新
+	});
+
+	cb();
+}
+
+watch(folder.src+'html/*',function(cb){
+	html();
+	cb();
+});
+watch(folder.src+'css/*',function(cb){
+	css();
+	cb();
+});
+watch(folder.src+'js/*',function(cb){
+	js();
+	cb();
+});
+
+exports.default = series(html, css, js, image, server)
--- src/js/index.js
+++
@@ -1,7 +0,0 @@
-console.log(123456);
-const arr = [1, 2, 3, 4, 5, 6];
-
-const res = arr.reduce(function (prev, cur) {
-    return prev + cur;
-});
-console.log(res);
